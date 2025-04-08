1de51cbe50b70f30d59157f5ce38c9691894bad9
Kingsley Leung <kingsleyleung2003@outlook.com>
2025-04-08 19:47:18 +1200

    Fix Build Issue - Related to Sitemap. - Temporarily return an empty array.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,16 +1,17 @@
 
 export const baseUrl = 'https://vcardcontactgenerator.vercel.app/'
 
-// export default async function sitemap() {
-//   let blogs = getBlogPosts().map((post) => ({
-//     url: `${baseUrl}/blog/${post.slug}`,
-//     lastModified: post.metadata.publishedAt,
-//   }))
+export default async function sitemap() {
+  // let blogs = getBlogPosts().map((post) => ({
+  //   url: `${baseUrl}/blog/${post.slug}`,
+  //   lastModified: post.metadata.publishedAt,
+  // }))
 
-//   let routes = ['', '/blog'].map((route) => ({
-//     url: `${baseUrl}${route}`,
-//     lastModified: new Date().toISOString().split('T')[0],
-//   }))
+  // let routes = ['', '/blog'].map((route) => ({
+  //   url: `${baseUrl}${route}`,
+  //   lastModified: new Date().toISOString().split('T')[0],
+  // }))
 
-//   return [...routes, ...blogs]
-// }
+  // return [...routes, ...blogs]
+  return []
+}
