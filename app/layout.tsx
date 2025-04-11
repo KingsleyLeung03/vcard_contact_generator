cce9141e808bf659b494450aeb49ea6ce9d692fd
Kingsley Leung <kingsleyleung2003@outlook.com>
2025-04-11 15:06:06 +1200

    Setup OG Image - Updated layout.tsx to include the OG image. - Added the drawio file for future use.

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,14 @@ export const metadata: Metadata = {
     siteName: 'vCard Contact Generator',
     locale: 'en_US',
     type: 'website',
+    images: [
+      {
+        url: '/og-image.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'vCard Contact Generator Open Graph Image',
+      },
+    ],
   },
   twitter: {
     card: 'summary_large_image',
@@ -30,7 +38,7 @@ export const metadata: Metadata = {
     // siteId: 'your_twitter_site_id', // To be added later
     creator: '@KingsleyLeung03',
     creatorId: '723487452988465152',
-    // images: ['/og-image.jpg'], // To be added later
+    images: ['/og-image.jpg'],
   },
   robots: {
     index: true,
