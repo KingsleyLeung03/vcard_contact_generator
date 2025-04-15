e5fb26391be80b06a22bbafd499aead1a3c91fda
Kingsley Leung <kingsleyleung2003@outlook.com>
2025-04-15 19:09:23 +1200

    Initialise Multiple Contact Page - Add DaisyUI as a dependency for future styling. - Create the initial structure for the multiple contact page at the `/multi` route. - Refactor the Navbar component:     - Rename it to "Header".     - Include the page title within the Header component.

---
+++ app/multi/page.tsx
@@ -0,0 +1,12 @@
+export default async function Page() {
+
+  return (
+    <section>
+
+        Multiple Contacts Page
+        <br />
+        TODO: Implement the multiple contacts page
+      
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,6 @@ export default async function Page() {
 
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        vCard Contact Generator
-      </h1>
       <ContactDisplay />
     </section>
   );
